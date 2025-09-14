dc6bfe9a8f3713287d536ef9ab87d5ef29503a80
sushmalamsall <burgeon.sushmalamsal@gmail.com>
2025-09-14 16:17:37 +0545

    initial_commit

--- lib/common/ui/custom_two_column_text.dart
+++ lib/common/ui/custom_two_column_text.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:yummy_bites/common/ui/custom_ui_helper.dart';
 
 class CustomTwoColumnText extends StatelessWidget {
   final String title;
